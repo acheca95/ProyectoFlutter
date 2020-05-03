d0a43fc8f8c9ccaf827b69082fe5d426ccca753e
Antonio Checa <antonio_ch_95@hotmail.com>
2020-05-03 20:31:10 +0200

    cambio Listado usuario
    se añade filtro de horas

--- lib/listado_usuario.dart
+++ lib/listado_usuario.dart
@@ -3,11 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+import 'package:intl/intl.dart';
 import 'package:jandulaseneca/sign_in.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
 import 'package:printing/printing.dart';
+import 'package:f_datetimerangepicker/f_datetimerangepicker.dart';
 
 class ListUser extends StatefulWidget {
   @override
@@ -17,8 +19,9 @@ class ListUser extends StatefulWidget {
 Firestore _firestore = Firestore.instance;
 Firestore _firestore2 = Firestore.instance;
 List<DocumentSnapshot> Profesores3 = [];
+List<DocumentSnapshot> Profesores6 = [];
 bool loadingProfes = true;
-
+var show = false;
 //busqueda
 DateTime selectedDate = DateTime.now();
 
@@ -176,27 +179,103 @@ class _ListPageState extends State<ListUser> {
                           subtitle: Text("Nombre: " + name),
                         ),
                         Expanded(
-                          child: ListView.builder(
-                              scrollDirection: Axis.vertical,
-                              shrinkWrap: true,
-                              itemCount: Profesores3.length,
-                              itemBuilder: (BuildContext ctx, int index) {
-                                return ListTile(
-                                  //se imprime la fecha de inicio y la de fin del usuario correspondiente.
-                                  title: Text("Fecha Inicio: " +
-                                      Profesores3[index].data["Fecha_inicio"]),
-                                  subtitle: Text("Fecha Fin: " +
-                                      Profesores3[index].data["Fecha_fin"]),
-                                );
-                              }),
+                          child: show == true && Profesores6.length != 0
+                              ? Center(
+                                  child: ListView.builder(
+                                      scrollDirection: Axis.vertical,
+                                      shrinkWrap: true,
+                                      itemCount: Profesores6.length,
+                                      itemBuilder:
+                                          (BuildContext ctx, int index) {
+                                        return ListTile(
+                                          //se imprime la fecha de inicio y la de fin del usuario correspondiente.
+                                          title: Text("Fecha Inicio: " +
+                                              Profesores6[index]
+                                                  .data["Fecha_inicio"]),
+                                          subtitle: Text("Fecha Fin: " +
+                                              Profesores6[index]
+                                                  .data["Fecha_fin"]),
+                                        );
+                                      }),
+                                )
+                              : ListView.builder(
+                                  scrollDirection: Axis.vertical,
+                                  shrinkWrap: true,
+                                  itemCount: Profesores3.length,
+                                  itemBuilder: (BuildContext ctx, int index) {
+                                    return ListTile(
+                                      //se imprime la fecha de inicio y la de fin del usuario correspondiente.
+                                      title: Text("Fecha Inicio: " +
+                                          Profesores3[index]
+                                              .data["Fecha_inicio"]),
+                                      subtitle: Text("Fecha Fin: " +
+                                          Profesores3[index].data["Fecha_fin"]),
+                                    );
+                                  }),
                         ),
                         const SizedBox(height: 30),
                       ],
                     ),
             ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () => _selectDate(context),
-        label: Text('Filtrar'),
+        onPressed: () {
+          Query q2 = _firestore2.collection("Asistencia").orderBy("Fecha_fin");
+
+          DateTime now = DateTime.now();
+          DateTime last = DateTime.now();
+          DateTimeRangePicker(
+              startText: "Inicio",
+              endText: "Fin",
+              doneText: "Aceptar",
+              cancelText: "Cancelar",
+              interval: 5,
+              initialStartTime: now.toLocal(),
+              initialEndTime: last.toLocal().add(Duration(days: 20)),
+              mode: DateTimeRangePickerMode.dateAndTime,
+              minimumTime:
+                  DateTime.now().toLocal().subtract(Duration(days: 25)),
+              maximumTime: DateTime.now().toLocal().add(Duration(days: 25)),
+              onConfirm: (start, end) async {
+                print(start);
+                print(end);
+                if (start.isBefore(end)) {
+                  show = true;
+                  print(new DateFormat('yyyy-MM-dd – kk:mm').format(start));
+                  print(new DateFormat('yyyy-MM-dd – kk:mm').format(end));
+                  Query q = _firestore
+                      .collection("Asistencia")
+                      .where("Nombre", isEqualTo: name)
+                      .where("Apellidos", isEqualTo: apellidos)
+                      .where("Fecha_inicio",
+                          isGreaterThanOrEqualTo:
+                              new DateFormat('yyyy-MM-dd – kk:mm')
+                                  .format(start))
+                      .where("Fecha_inicio",
+                          isLessThanOrEqualTo:
+                              new DateFormat('yyyy-MM-dd – kk:mm').format(end));
+
+                  QuerySnapshot querySnapshot = await q.getDocuments();
+                  Profesores6 = querySnapshot.documents;
+                  for (int i = 0; i < Profesores6.length; i++) {
+                    print(Profesores6[i]);
+                  }
+                  // navigateToDetail(start, end, Profesores6[Profesores6.length]);
+                } else {
+                  showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text("Error"),
+                          content: Text(
+                              "La fecha de fin debe ser posterior a la inicial."),
+                        );
+                      });
+                }
+              }).showPicker(context);
+        },
+        label: Center(
+          child: Text("Filtrar "),
+        ),
         icon: Icon(Icons.date_range),
         backgroundColor: Colors.deepPurpleAccent,
       ),
